e7a3f8721bae71491fe45010d217f52acccbfc4a
Rhaiany Souza <94574398+RhaianySouza@users.noreply.github.com>
2023-04-09 01:16:57 -0300

    Update and rename cauculadoraareaevol.js to calculadora_areas_e_volumes.js

--- calculadora_areas_e_volumes.js
+++ calculadora_areas_e_volumes.js
@@ -7,16 +7,16 @@ class cubo{
       return this.lado**2;
     }else{
       this.lado = A**(1/2);
-    }
+    };
   };
   volume(V){
   	if(V == null){
       return this.lado**3;
     }else{
       this.lado = V**(1/2);
-    }
-  }
-}
+    };
+  };
+};
 
 class cilindro{
   constructor(raio,diametro,altura){
@@ -49,7 +49,7 @@ class cilindro{
       this.altura = V(this.PI*this.raio**2);
     };
   };
-}
+};
 
 class esfera{
   constructor(raio,diametro,altura){
@@ -65,29 +65,36 @@ class esfera{
   }
 }
 class cone{
-  constructor(raio,diametro,altura){
+  constructor(raio,diametro,altura,geratriz){
     this.PI = Math.PI;
-    this.raio = raio==null?diametro/2:raio;
-    this.diametro = diametro==null?raio*2:diametro;
-    this.geratriz = geratriz==null?
-  }
+    this.raio = raio==null? (diametro!=null? diametro/2: null): raio;
+    this.diametro = diametro==null? (raio!=null? raio*2: null): diametro;
+    this.altura = altura==null? (geratriz!=null?((this.raio**2)*(this.geratriz**2))**(1/2):null):altura;
+    this.geratriz = geratriz==null? (altura!=null?((this.raio**2)*(this.altura**2))**(1/2):null):geratriz;
+  };
   area(A){
     return this.PI*this.raio*(this.raio+this.geratriz);
   };
   volume(V){
   	return (this.PI*(this.raio**2)*this.geratriz)/3;
-  }
-}
+  };
+};
 
 class piramide{
-  constructor(base,altura,altura){
-    this.base = base;
+  constructor(lado1, lado2, altura, vertices, precisao){
+    this.lado1 = lado1;
+    this.lado2 = lado2;
     this.altura = altura;
-  }
+    this.vertices = vertices;
+    this.precisao = precisao;
+  };
   area(A){
-    return (this.base*this.altura)/2;
+    return (this.vertices*this.altura)/2;
+  };
+  areaBase(){
+    return this.lado1*this.lado2/2*this.vertices;
   };
   volume(V){
-  	return ;
-  }
-}
+  	return (this.areaBase()*this.altura)/3 ;
+  };
+};
